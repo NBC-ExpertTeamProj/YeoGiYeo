f811772d2ff6478dc85dbf7ba164abacd863a8ea
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-06-17 21:55:49 +0900

    Setup: Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Router from './shared/Router';
+import { AppContainer } from './styles/GlobalStyles/AppStyle';
+
+function App() {
+  return (
+    <>
+      <AppContainer>
+        <Router />
+      </AppContainer>
+    </>
+  );
+}
+
+export default App;
