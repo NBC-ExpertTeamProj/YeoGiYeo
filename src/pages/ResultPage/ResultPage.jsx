bb4bd508d65dd4fccdec7321534c064e305317b5
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-06-18 11:10:56 +0900

    add:SharedLink Feature

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -1,4 +1,8 @@
+import LinkShare from '../../components/ResultPageComp/LinkShare';
+
 const ResultPage = () => {
+  const url = 'https://YeoGiYeo.com';
+  const text = '배포할 내용';
   return (
     <>
       <div>
@@ -11,6 +15,10 @@ const ResultPage = () => {
       <div>
         <button>음식 이름과 가장 어울리는 디저트는?</button>
         <button>음식 이름과 가장 어울리는 주종은?</button>
+      </div>
+      <div>
+        <h1>Share this page</h1>
+        <LinkShare platform={Kakao} url={url} text={text} />
       </div>
       <div>youtubeapi</div>
       <div>mapapi</div>
