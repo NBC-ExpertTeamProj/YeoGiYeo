f42604875a434aa07b0afb34aea175dda648e47e
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-06-18 10:04:32 +0900

    update:resultPage format

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -1,5 +1,21 @@
 const ResultPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <h2>메뉴 추천 결과</h2>
+      </div>
+      <div>
+        <h3>설문 조사 결과 추천 메뉴는...</h3>
+        <p>음식 이름</p>
+      </div>
+      <div>
+        <button>음식 이름과 가장 어울리는 디저트는?</button>
+        <button>음식 이름과 가장 어울리는 주종은?</button>
+      </div>
+      <div>youtubeapi</div>
+      <div>mapapi</div>
+    </>
+  );
 };
 
 export default ResultPage;
