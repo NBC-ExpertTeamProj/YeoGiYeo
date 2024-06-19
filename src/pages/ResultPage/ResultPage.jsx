c970f8e22dc7dae1280d976e750c62d68d341160
poop6028@naver.com <poop6028@naver.com>
2024-06-19 16:02:02 +0900

    add : youtube style

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -2,7 +2,49 @@ import React, { useState } from 'react';
 import LinkShare from '../../components/ResultPageComp/LinkShare';
 import RandomSuggestion from '../../components/ResultPageComp/RandomSuggestion';
 import { viewVideos } from '../../api/YoutubeApi/YoutubeApi';
+import styled from 'styled-components';
 
+const YoutubeCard = styled.div`
+  margin-top: 20px;
+  padding: 20px;
+`;
+const YoutubeVideoList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 10px;
+  height: 100%;
+`;
+
+const YoutubeVideo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid #ccc;
+  padding: 15px;
+  cursor: pointer;
+  border-radius: 10px;
+  margin: 0 auto;
+  width: 220px;
+  margin-left: 120px;
+`;
+
+const YoutubeTitle = styled.h3`
+  font-size: 15px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const YoutubeThumbnail = styled.img`
+  margin-top: 10px;
+  width: 100%;
+`;
+const WatchVideo = styled.div`
+  margin-top: 20px;
+`;
+const VideoTitle = styled.h2`
+  margin-bottom: 10px;
+`;
 const ResultPage = () => {
   const [food, setFood] = useState(null);
   const [error, setError] = useState(null);
@@ -123,7 +165,7 @@ const ResultPage = () => {
             ></iframe>
           </WatchVideo>
         )}
-      </div>
+      </YoutubeCard>
       <div>mapapi</div>
     </>
   );
