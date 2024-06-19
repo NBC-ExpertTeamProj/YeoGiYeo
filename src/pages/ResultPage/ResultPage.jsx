d4f5beb3602f34d7700484ffb59db735a3cd9a69
poop6028@naver.com <poop6028@naver.com>
2024-06-19 15:57:28 +0900

    add : youtube styel

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -1,7 +1,50 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import LinkShare from '../../components/ResultPageComp/LinkShare';
 import RandomSuggestion from '../../components/ResultPageComp/RandomSuggestion';
 import { viewVideos } from '../../api/YoutubeApi/YoutubeApi';
+import styled from 'styled-components';
+
+const YoutubeCard = styled.div`
+  margin-top: 20px;
+  padding: 20px;
+`;
+const YoutubeVideoList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 10px;
+  height: 100%;
+`;
+
+const YoutubeVideo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid #ccc;
+  padding: 15px;
+  cursor: pointer;
+  border-radius: 10px;
+  margin: 0 auto;
+  width: 220px;
+  margin-left: 120px;
+`;
+
+const YoutubeTitle = styled.h3`
+  font-size: 15px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const YoutubeThumbnail = styled.img`
+  margin-top: 10px;
+  width: 100%;
+`;
+const WatchVideo = styled.div`
+  margin-top: 20px;
+`;
+const VideoTitle = styled.h2`
+  margin-bottom: 10px;
+`;
 
 const ResultPage = () => {
   const [videos, setVideos] = useState([]);
@@ -38,7 +81,7 @@ const ResultPage = () => {
         <h1>Share this page</h1>
         <LinkShare />
       </div>
-      <div>
+      <YoutubeCard>
         <form onSubmit={handleSearch}>
           <input
             type="text"
@@ -48,30 +91,29 @@ const ResultPage = () => {
           />
           <button type="submit">검색</button>
         </form>
-        <div>
+        <YoutubeVideoList>
           {videos.map((video) => (
-            <div key={video.id.videoId} onClick={() => handleVideoSelect(video)}>
-              <h3>{video.snippet.title}</h3>
-              <img src={video.snippet.thumbnails.medium.url} alt={`${video.snippet.title}의 썸네일`} />
-            </div>
+            <YoutubeVideo key={video.id.videoId} onClick={() => handleVideoSelect(video)}>
+              <YoutubeTitle>{video.snippet.title}</YoutubeTitle>
+              <YoutubeThumbnail src={video.snippet.thumbnails.medium.url} alt={`${video.snippet.title}의 썸네일`} />
+            </YoutubeVideo>
           ))}
-        </div>
+        </YoutubeVideoList>
         {selectedVideo && (
-          <div>
-            <h2>{selectedVideo.snippet.title}</h2>
+          <WatchVideo>
+            <VideoTitle>{selectedVideo.snippet.title}</VideoTitle>
             <iframe
-              width="100%"
-              height="400"
+              width="90%"
+              height="750"
               src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
               title="YouTube video player"
             ></iframe>
-            <p>{selectedVideo.snippet.description}</p>
-          </div>
+          </WatchVideo>
         )}
-      </div>
+      </YoutubeCard>
       <div>mapapi</div>
     </>
   );
